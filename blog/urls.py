c630b4ab08b9c2ffaae917f7671bff8c3ed4cef3
Yamir <yamircueva11052005@gmail.com>
2025-05-10 19:19:06 -0500

    Agregadas vistas y plantilla para el detalle del post del blog así como también CSS para el sitio.

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
 
-# Aca importamos el archivo views.py, que es donde tenemos las vistas de nuestra app blog 
-
 urlpatterns = [
-    path('', views.post_list, name='post_list'), # Importamos la vista post_list de views.py y le asignamos la url /
+    path('', views.post_list, name='post_list'),  # Ruta para la lista de posts
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),  # Ruta para el detalle de un post
 ]
